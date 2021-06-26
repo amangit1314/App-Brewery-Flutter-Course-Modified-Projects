48d67b0c448f19431e69363fe725b121a9c49099
Aman Soni <gitaman8481@gmail.com>
2021-06-26 10:03:37 +0530

    xylophone completed

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,18 +1,50 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue[900],
+            actions: <Widget>[
+              Image(
+                image: AssetImage(''),
+              ),
+            ],
+            title: Text('Ask Me Anything'),
+          ),
+          backgroundColor: Colors.blue,
+          body: Ball(),
+        ),
+      ),
+    );
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
 }
 
-class MyApp extends StatelessWidget {
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  ballNumber = Random().nextInt(4) + 1;
+                });
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
